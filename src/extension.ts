ec754213055bd76dc689210538b27366033ffeca
developerjhp <developerjhp@gmail.com>
2025-06-10 01:36:24 +0900

    feat : quick info on cursor

---
+++ src/extension.ts
@@ -0,0 +1,34 @@
+import * as vscode from 'vscode';
+
+let hoverTimer: NodeJS.Timeout | undefined;
+
+export function activate(context: vscode.ExtensionContext) {
+  const selectionChangeDisposable = vscode.window.onDidChangeTextEditorSelection((event) => {
+    if (!vscode.window.activeTextEditor) {
+      return;
+    }
+
+    const config = vscode.workspace.getConfiguration('quickInfoOnCursor');
+    if (!config.get('enabled', true)) {
+      return;
+    }
+
+    if (hoverTimer) {
+      clearTimeout(hoverTimer);
+    }
+
+    const delay = config.get('delay', 500);
+
+    hoverTimer = setTimeout(() => {
+      vscode.commands.executeCommand('editor.action.showHover');
+    }, delay);
+  });
+
+  context.subscriptions.push(selectionChangeDisposable);
+}
+
+export function deactivate() {
+  if (hoverTimer) {
+    clearTimeout(hoverTimer);
+  }
+}
